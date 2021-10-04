64b788374c3d5972255f158976695c95ec2bc289
Roman Stalmakov <roman_stalmakov@mail.ru>
2021-10-04 12:59:35 +0000

    Update ElectronicWatch.java

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -9,11 +9,14 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        SimpleDateFormat df = new SimpleDateFormat("H:mm:ss");
         Calendar calendar = new GregorianCalendar();
+
         calendar.set(Calendar.HOUR, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, seconds);
-        SimpleDateFormat df = new SimpleDateFormat("H:mm:ss");
+        calendar.set(Calendar.AM_PM, 0);
+
         System.out.println(df.format(calendar.getTime()));
     }
 }
